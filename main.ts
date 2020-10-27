fabd46dcf9d86857317b75a71cd09f41e4138d4a
yun921 <71689256+yun921@users.noreply.github.com>
2020-10-27 11:01:47 +0800

    終於把下半部寫完了啦乾

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 basic.forever(function () {
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= x; y++) {
-            led.plot(x, y)
-            basic.pause(200)
+            led.plot(4 - x + y, y)
+            basic.pause(100)
+        }
+    }
+    for (let x = 0; x <= 3; x++) {
+        for (let y = 0; y <= 4; y++) {
+            led.plot(-1 - x + y, y)
+            basic.pause(100)
         }
     }
 })
